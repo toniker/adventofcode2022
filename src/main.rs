eb44ba1dd6496ceaac4bd2970e103c95a0d55b08
Antonis Keremidis <tkeremidis@gmail.com>
2022-12-03 20:22:00 +0200

    Day 2 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,84 @@
 use std::fs;
+use crate::Hand::{Paper, Rock, Scissors};
+
+#[derive(PartialEq)]
+enum Hand {
+    Rock,
+    Paper,
+    Scissors
+}
+
+trait Beats {
+    fn beats(&self) -> Self;
+}
+
+impl Beats for Hand {
+    fn beats(&self) -> Self {
+        match *self {
+            Paper => Rock,
+            Rock => Scissors,
+            Scissors => Paper,
+        }
+    }
+}
+
+trait Points {
+    fn points(&self) -> i32;
+}
+
+impl Points for Hand {
+    fn points(&self) -> i32 {
+        match *self {
+            Rock => { 1 }
+            Paper => { 2 }
+            Scissors => { 3 }
+        }
+    }
+}
+struct Game {
+    my_move: Hand,
+    opponent_move: Hand
+}
+
+fn parse_input(opponent_move: char, my_move: char) -> Game {
+    let mut game: Game = Game { my_move: Hand::Rock, opponent_move: Hand::Rock };
+    game.opponent_move = match opponent_move {
+        'A' => Rock,
+        'B' => Paper,
+        'C' => Scissors,
+        _ => { Rock }
+    };
+    game.my_move = match my_move {
+        'X' => Rock,
+        'Y' => Paper,
+        'Z' => Scissors,
+        _ => { Rock }
+    };
+    game
+}
+
+fn get_points(game: Game) -> i32 {
+    if game.my_move.beats() == game.opponent_move {
+        6 + game.my_move.points()
+    } else if game.opponent_move.beats() == game.my_move {
+        game.my_move.points()
+    } else {
+        3 + game.my_move.points()
+    }
+}
 
 fn main() {
     let file_path = "src/input.txt";
 
     let input = fs::read_to_string(file_path).expect("File not found");
+    let games = input.split('\n');
 
-    let elves = input.split("\n\n");
-
-    let mut elf_calories: Vec<i32> = Vec::new();
-
-    for elf in elves {
-        let mut count: i32 = 0;
-        let items = elf.split('\n');
-        for item in items {
-            count += match item.parse::<i32>() {
-                Ok(n) => { n }
-                Err(_) => { 0 }
-            }
-        }
-        elf_calories.push(count);
+    let mut my_score: i32 = 0;
+    for game in games {
+        let char_vec: Vec<char> = game.chars().collect();
+        let game = parse_input(char_vec[0], char_vec[2]);
+        my_score += get_points(game);
     }
 
-    elf_calories.sort();
-    let len: usize = elf_calories.len();
-
-    println!("Elf with most calories: {:?}", elf_calories[len - 1]);
-
-    let sum_of_top_3: i32 = elf_calories[len - 1] + elf_calories[len - 2] + elf_calories[len - 3];
-
-    println!("Sum of top 3 elves with most calories: {:?}", sum_of_top_3);
+    println!("{:?}", my_score);
 }
