2496b58c61bb76536efa3429e444e5e87ad8b88a
Antonis Keremidis <tkeremidis@gmail.com>
2022-12-07 21:23:57 +0200

    Complete day 4

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,26 @@ fn is_contained(assignment1: Assignment, assignment2: Assignment) -> bool {
         struct_is_contained
 }
 
+fn does_overlap(assignment1: Assignment, assignment2: Assignment) -> bool {
+    let mut overlap: bool = false;
+
+    if assignment1.low <= assignment2.low && assignment1.high >= assignment2.low {
+        overlap = true;
+    }
+    if assignment2.low <= assignment1.low && assignment2.high >= assignment1.low {
+        overlap = true;
+    }
+
+    overlap
+}
+
 fn main() {
     let file_path = "src/input.txt";
     let input = fs::read_to_string(file_path).expect("File not found");
     let pairs = input.lines();
 
     let mut fully_contained: i32 = 0;
+    let mut overlapping: i32 = 0;
 
     for pair in pairs {
         let mut assignment_vec: Vec<Assignment> = Vec::new();
@@ -37,7 +51,12 @@ fn main() {
         if is_contained(assignment_vec[0], assignment_vec[1]) {
             fully_contained += 1;
         }
+        if does_overlap(assignment_vec[0], assignment_vec[1]) {
+            overlapping += 1;
+        }
+
     }
 
-    println!("{fully_contained}");
+    println!("Fully contained: {fully_contained}");
+    println!("Overlapping: {overlapping}");
 }
